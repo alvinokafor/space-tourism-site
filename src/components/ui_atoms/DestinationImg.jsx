e50f542a3db655e5cb7fd66788a748c545287d8c
Alvin Okafor <alvin2k99@gmail.com>
2022-11-11 21:22:02 +0100

    fix: image loading

--- src/components/ui_atoms/DestinationImg.jsx
+++ src/components/ui_atoms/DestinationImg.jsx
@@ -1,7 +1,5 @@
-import Mars from '../../assets/destination/image-mars.png'
-
 export default function DestinationImg({image}) {
   return (
-    <div className='animate-spin-slow lg:w-96 lg:h-96  mx-auto md:w-72 md:h-72 w-44 h-44 mt-8'><img className='w-full' src={image.png} alt="Mars Image" /></div>
+    <div className='animate-spin-slow lg:w-96 lg:h-96  mx-auto md:w-72 md:h-72 w-44 h-44 mt-8'><img className='w-full' src={image.png}  /></div>
   )
 }
